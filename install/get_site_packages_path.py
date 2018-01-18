daf2e801851cc6fbc79afca38bc48e98a2caa6a0
elliott_trade_labo <matsumura.shigeru.225@gmail.com>
2018-01-19 07:55:54 +0900

    update

--- install/get_site_packages_path.py
+++ install/get_site_packages_path.py
@@ -3,7 +3,7 @@ import os
 import re
 
 for path in sys.path:
-    if re.match(r'^.*site-packages/alewrap_py-[\d]\.[\d]\.[\d]-py[\d]\.[\d]\.egg$', path):
+    if re.match(r'^.*(site|dist)-packages/alewrap_py-[\d]\.[\d]\.[\d]-py[\d]\.[\d]\.egg$', path):
         print(re.sub(r'alewrap_py-[\d]\.[\d]\.[\d]-py[\d]\.[\d]\.egg', '', path))
         sys.exit(0)
 print('you have not install alewrap_py')
