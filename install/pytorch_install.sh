8cfedee9c5d8c2afe1d80453521fe70c171dc96a
elliott_trade_labo <matsumura.shigeru.225@gmail.com>
2018-03-21 18:46:06 +0900

    update

--- install/pytorch_install.sh
+++ install/pytorch_install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 echo Install PyTorch
 
-pip3 install http://download.pytorch.org/whl/cu80/torch-0.3.0.post4-cp35-cp35m-linux_x86_64.whl
-#pip3 install http://download.pytorch.org/whl/cu90/torch-0.3.0.post4-cp35-cp35m-linux_x86_64.whl
+pip3 install http://download.pytorch.org/whl/cu90/torch-0.3.1-cp35-cp35m-linux_x86_64.whl 
 pip3 install torchvision
-pip3 install https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-1.4.1-cp35-cp35m-linux_x86_64.whl
+pip3 install https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-1.6.0-cp35-cp35m-linux_x86_64.whl
