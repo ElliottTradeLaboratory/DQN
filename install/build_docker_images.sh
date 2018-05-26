819f1381988a4fd94919b493d230e34cf6bf9273
elliott_trade_labo <matsumura.shigeru.225@gmail.com>
2018-05-26 18:56:51 +0900

    update

--- install/build_docker_images.sh
+++ install/build_docker_images.sh
@@ -3,7 +3,7 @@
 
 echo "Build Docker image for PyTorch, Tensorflow, CNTK"
 cp Dockerfile_cuda8_cudnn6 Dockerfile
-nvidia-docker build -t dqn_test --build-arg FRAMEWORK"=pytorch tensorflow cntk" .
+nvidia-docker build -t dqn_test --no-cache --build-arg FRAMEWORK"=pytorch tensorflow cntk" .
 
 #echo "Build Docker image for MXNet"
 #cp Dockerfile_cuda8_cudnn5 Dockerfile
