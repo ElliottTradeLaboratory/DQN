e9249b1edb51a7710288c3afd4d4970497fccfb6
elliott_trade_labo <matsumura.shigeru.225@gmail.com>
2018-05-26 21:49:12 +0900

    update

--- install/build_docker_images.sh
+++ install/build_docker_images.sh
@@ -8,3 +8,5 @@ nvidia-docker build -t dqn --no-cache --build-arg FRAMEWORK"=pytorch tensorflow 
 echo "Build Docker image for MXNet"
 cp Dockerfile_cuda8_cudnn5 Dockerfile
 nvidia-docker build -t dqn_for_mxnet --build-arg FRAMEWORK=mxnet .
+
+docker images
