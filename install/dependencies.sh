426bca4f718540ad56de98fc1ab6d664b3ffd55a
elliott_trade_labo <matsumura.shigeru.225@gmail.com>
2018-05-26 19:50:02 +0900

    update

--- install/dependencies.sh
+++ install/dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 apt-get update
 apt-get install -y python-numpy python3-dev cmake zlib1g-dev libjpeg-dev xvfb libav-tools xorg-dev python-opengl libboost-all-dev libsdl2-dev swig zip python3-tk python3-pip 
-pip install --upgrade pip
+pip3 install --upgrade pip
 pip install --upgrade setuptools
